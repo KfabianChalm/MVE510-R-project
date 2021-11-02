c0ef9ec4760b879ad3dfc90916851e3173863d7f
KfabianChalm <90684436+KfabianChalm@users.noreply.github.com>
2021-11-02 22:01:47 +0100

    Add files via upload

---
+++ Exercise 1/Part1.R
@@ -0,0 +1,9 @@
+#Quit the program
+#q()
+
+#Get help on q#Alternatively: ?q
+
+#Loads MASS package into library
+library(MASS)
+#Opens web page illustrating all Vignettes in different packages
+browseVignettes()
